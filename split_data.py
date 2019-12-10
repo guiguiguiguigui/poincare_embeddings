3b534e6bf4c727e7b1e28d0c655232748f84781b
Sophia Sun <sophiaphia@live.com>
2019-12-09 20:31:07 -0800

    yes

--- split_data.py
+++ split_data.py
@@ -1,7 +1,7 @@
 import pandas, argparse
-from sklearn.model_selection import KFold
+from sklearn.model_selection import KFold, train_test_split
 
-def main():
+def main1():
     parser = argparse.ArgumentParser(description='Train Hyperbolic Embeddings')
     parser.add_argument('-dset', type=str, required=True,
                         help='Dataset identifier')
@@ -22,5 +22,23 @@ def main():
         df.iloc[train_index].to_csv(train_csv.format(count), index=False)
         df.iloc[test_index].to_csv(test_csv.format(count), index=False)
         count += 1
+
+def main():
+    parser = argparse.ArgumentParser(description='Train Hyperbolic Embeddings')
+    parser.add_argument('-dset', type=str, required=True,
+                        help='Dataset identifier')
+    parser.add_argument('-out', type=str, required=True,
+                        help='out path')
+    opt = parser.parse_args()
+
+    df = pandas.read_csv(opt.dset, usecols=['id1', 'id2', 'weight'], engine='c')
+    X_train, X_test = train_test_split(df, test_size = 0.3, random_state = 42)
+
+    train_csv = "resources/" + opt.out + "/train_{}.csv"
+    test_csv = "resources/" + opt.out + "/test_{}.csv"
+
+    X_train.to_csv(train_csv.format(30), index=False)
+    X_test.to_csv(test_csv.format(30), index=False)
+
 if __name__ == '__main__':
     main()
